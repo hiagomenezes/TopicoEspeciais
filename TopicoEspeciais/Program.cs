aee40ad86a8c2468f5db72e31688f455fe55440e
hiago roberto <hiago@itecnologia.com.br>
2019-12-19 17:55:17 -0300

    teste de topicos especiais projeto udemy

---
+++ TopicoEspeciais/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopicoEspeciais.Extensions;
+
+namespace TopicoEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2019, 12, 19, 17, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
